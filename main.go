b5dd4fab1712a30fed1a5d85317490c6d832d6b5
Siddharth Lakhara <siddharth.lakhara@gmail.com>
2025-05-14 17:22:56 +0530

    add comments

--- main.go
+++ main.go
@@ -4,12 +4,17 @@ import (
 	"fmt"
 )
 
+// Entry point for web assembly
+// Function can be empty, but definition is required
+func main() {
+	fmt.Println("Hello from Go files")
+}
+
+// Compiler directive for golang compiler
+// syntax: go:wasmexport <export-name>
+//
 //go:wasmexport hello
 func hello() *byte {
 	msg := "Hello, WebAssembly!"
 	return &[]byte(msg)[0]
-}
-
-func main() {
-	fmt.Println("Hello from Go files")
 }
